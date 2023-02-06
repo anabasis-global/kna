d6a4e3373fa4befb634d18135b735d34aeb35a74
CodeSandbox <support@codesandbox.io>
2023-02-06 20:15:54 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client ";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Script from "next/script";
+
+import {Form } from 'antd'
+
+export default function Home() {
+  return (
+    <div>
+      <form>
+        <Form />
+      </form>
+    </div>
+  );
+}
